43096e83e5166439af636e3953ecc233598aa407
Felipe Amorim <github@felipeamorim.xyz>
2020-07-10 21:20:09 -0400

    Add black and blue colors

--- config.def.h
+++ config.def.h
@@ -8,18 +8,22 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const char *fonts[]          = { "Inconsolata Nerd Font Mono:style=Regular:pixelsize=15:antialias=true:autohint=true" };
+static const char *fonts[]          = {
+  "Inconsolata Nerd Font Mono:style=Regular:pixelsize=15:antialias=true:autohint=true",
+  "Font Awesome 5 Free:style=Regular:pixelsize=15:antialias=true:autohint=true",
+};
+
 static const char dmenufont[]       = "Inconsolata:size=13";
 
-static const char col_gray1[]       = "#222222"; /* backgruond color */
-static const char col_gray2[]       = "#444444"; /* inactive window border color */
-static const char col_gray3[]       = "#bbbbbb"; /* font color */
-static const char col_gray4[]       = "#eeeeee"; /* current tag and current window font color */
-static const char col_cyan[]        = "#ffcc66"; /* top bar second color and active window border color */
+static const char col_gray1[]       = "#111";       /* backgruond color */
+static const char col_gray2[]       = "#444";       /* inactive window border color */
+static const char col_gray3[]       = "#e6e6e6";    /* font color */
+static const char col_gray4[]       = "#78c";       /* current tag and current window font color */
+static const char col_cyan[]        = "#111";       /* top bar second color and active window border color */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray4 },
 };
 
 /* tagging */
