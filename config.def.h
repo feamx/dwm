681d39863fa1852b23a96c5cf82fa578d30ab3f3
Felipe Amorim <github@felipeamorim.xyz>
2020-07-12 18:13:27 -0400

    Implemente fullscreen

--- config.def.h
+++ config.def.h
@@ -78,6 +78,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,                      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,                      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,                      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_f,                      fullscreen,     {0} },
 	{ MODKEY|ShiftMask,             XK_Return,                 zoom,           {0} },
 	{ Mod1Mask,                     XK_Tab,                    view,           {0} },
 	{ MODKEY,                       XK_q,                      killclient,     {0} },
